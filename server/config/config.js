08908bd92b54d05d4628d1f94d36ffa3559a4291
Jorge Usma <jorgeusma@gmail.com>
2019-03-25 17:30:43 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:d1m3d3ll1n@ds157390.mlab.com:57390/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
